6a82c4ef16b7556767a8df75be0417ca3f9a06ec
swaggimus_grime <hunter66888@gmail.com>
2021-12-23 14:58:00 -0500

    Project restructure

--- compileShaders.bat
+++ compileShaders.bat
@@ -1,4 +1,4 @@
-call vendor\bin\Vulkan\glslc.exe Raydriarch\res\shaders\Basic.vert -o Raydriarch\res\shaders\vert.spv
-call vendor\bin\Vulkan\glslc.exe Raydriarch\res\shaders\Basic.frag -o Raydriarch\res\shaders\frag.spv
+call Vulkan\glslc.exe Raydriarch\res\shaders\Basic.vert -o Raydriarch\res\shaders\vert.spv
+call Vulkan\glslc.exe Raydriarch\res\shaders\Basic.frag -o Raydriarch\res\shaders\frag.spv
 
 PAUSE
